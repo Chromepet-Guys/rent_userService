fbc585f431dc3e231429e8275dbe8927129c3ee8
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-04-09 01:22:06 -0400

    Code Checkin

---
+++ app.js
@@ -0,0 +1,39 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/index');
+const error = require('./common/error'); 
+require('dotenv/config');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger/swagger.yaml');
+
+const app = express();
+
+winston.add(new winston.transports.File({ filename:'logfile.log' }));
+winston.add(new winston.transports.MongoDB({ db:process.env.DB_CONNECTION }));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/userService/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/userService',  userRoute);
+app.use(error);
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
+.then(() => {
+    console.log("Connected to DB");})
+.catch((err) => {console.log("Connection to DB was wrong.");})
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+if(process.env.NODE_ENV != "test")
+{
+    app.listen(process.env.SERVICE_PORT);
+    console.log("User Service Running On : " + process.env.SERVICE_PORT);
+}
+module.exports = app;
---
+++ services/userService.js
@@ -0,0 +1,84 @@
+const User = require('../models/User');
+const ObjectID = require('mongodb').ObjectID;
+
+const getUsers = async () => {
+    try {
+        const users = await User.find();
+        return users;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const getUser = async (_id) => {
+    try {
+        const user = await User.findOne({_id});
+        return user;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const saveUser = async (user) => {
+    try {
+        const userData = new User(user);
+        const savedUser = await userData.save();
+        return savedUser;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const updateUser = async (user) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate({_id : ObjectID(user._id)}, { $set: user });
+        return updatedUser;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const deleteUser = async (_id) => {
+    try {
+        const deletedUser = await User.deleteOne({ _id });
+        return deletedUser;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const authenticateUser = async (userName, password) => {
+    try {
+        const user = await User.findOne({ UserName: userName, Password: password })
+        return user;
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+const checkUserAlreadyExists = async (UserName) =>{
+    try{
+        const user = await User.findOne({UserName});
+        return user;
+    }
+    catch(err){
+        throw err;
+    }
+}
+
+
+module.exports = {
+    getUsers,
+    getUser,
+    saveUser,
+    updateUser,
+    deleteUser,
+    authenticateUser,
+    checkUserAlreadyExists
+}
